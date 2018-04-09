ca8f80c001a619ff9d0308ec2af5b235129e9cde
Ernest <ernest.bruno94@yahoo.com>
2018-04-09 11:12:59 -0700

    Add admin, game, landing page, user-panel components and game + user services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UserPanelComponent } from './user-panel/user-panel.component';
+import { GameComponent } from './game/game.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    UserPanelComponent,
+    GameComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
