14a97c4d802b0e82a832d8a38af41d4bd39f1563
Ernest <ernest.bruno94@yahoo.com>
2018-04-10 16:10:55 -0700

    Edit app routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -21,13 +21,6 @@ const appRoutes: Routes = [
     component: UserPanelComponent
   },
   {
-<<<<<<< HEAD
-=======
-    path: 'user/display',
-    component: ChatComponent
-  },
-  {
->>>>>>> be221f5e9d67418380cb92a194d2c826cc38e243
     path: 'game/create',
     component: GameComponent
   }
