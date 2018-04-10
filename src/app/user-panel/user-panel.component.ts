a6db0cba88e43dad0c4ddc264aea8f4f6623beda
Justin Lardani <jlardani93@gmail.com>
2018-04-10 14:00:12 -0700

    gets rid of gameService.

--- src/app/user-panel/user-panel.component.ts
+++ src/app/user-panel/user-panel.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 // import { User } from '../user.model';
 
 import { Router } from '@angular/router';
-import { UserService } from '../user.service';
 import { FirebaseListObservable } from 'angularfire2/database';
 
 
@@ -10,14 +9,13 @@ import { FirebaseListObservable } from 'angularfire2/database';
   selector: 'app-userpanel',
   templateUrl: './user-panel.component.html',
   styleUrls: ['./user-panel.component.css'],
-  providers: [UserService]
 })
 
 export class UserPanelComponent implements OnInit {
   shows: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
 
-  constructor(private router: Router, private userService: UserService){}
+  constructor(private router: Router){}
 
   ngOnInit(){
 
