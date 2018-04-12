015aa4f2ed1b3274824ee7e9facc1a9d4c7c8c4f
Justin Lardani <jlardani93@gmail.com>
2018-04-12 10:38:01 -0700

    inactive games are wiped from database.

--- src/app/user-panel/user-panel.component.ts
+++ src/app/user-panel/user-panel.component.ts
@@ -26,10 +26,12 @@ export class UserPanelComponent implements OnInit {
   user: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
   uid: string;
+  allGames: FirebaseListObservable<any[]>;
 
   constructor(private router: Router, private route: ActivatedRoute, private location: Location, private playerService: PlayerService, private queueService: QueueService){}
 
   ngOnInit(){
+    this.queueService.cleanGames();
     this.route.params.forEach(parameter=>{
       this.uid = parameter['uid'];
       console.log("This is the playerKey from the list of route parameters: " + this.uid);
@@ -37,9 +39,10 @@ export class UserPanelComponent implements OnInit {
       this.playerService.currentGameState.subscribe(state=>{
         console.log("This is the state: " + state);
         console.log(state.$value);
-        if (state.$value) {
+        console.log(state.$value !== -1);
+        if (state.$value !== -1) {
+          console.log("My state value is:" + state.$value)
           this.router.navigate(['game', 'display', state.$value]);
-          window.location.reload(); 
         }
       })
     })
